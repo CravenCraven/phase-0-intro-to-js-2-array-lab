629f6c78b4f5ba95b12b18ba6a376bf14d30d6ba
nicolecraven <craventhegreat@gmail.com>
2023-09-15 11:57:48 -0400

    update lab

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+destructivelyPrependCat("Whiskers");
+function appendCat(name) {
+    const newCats = cats.slice(); 
+    newCats.push(name); 
+    return newCats;
+  }
+  
+  function prependCat(name) {
+    const newCats = cats.slice();
+    newCats.unshift(name);
+    return newCats;
+  }
+  function removeLastCat() {
+    const newCats = cats.slice(); 
+    newCats.pop(); 
+    return newCats;
+  }
+  function removeFirstCat() {
+    const newCats = cats.slice(); 
+    newCats.shift();
+    return newCats; 
+  }
+  function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  const newCats1 = appendCat("Whiskers");
+const newCats2 = prependCat("Socks");
+const newCats3 = removeLastCat();
+const newCats4 = removeFirstCat();
+
+console.log("Original cats array:", cats);
+console.log("After appending:", newCats1);
+console.log("After prepending:", newCats2);
+console.log("After removing last cat:", newCats3);
+console.log("After removing first cat:", newCats4);
